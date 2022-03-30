9305dd15794a1bc776ba996cab7a5900cac8f5ae
bonnie <arlolocos@gmail.com>
2022-03-30 13:24:44 -0400

    new styles

--- server.js
+++ server.js
@@ -3,15 +3,4 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/2", (req, res) => {
-  res.sendFile(__dirname + "/public/theme2/index.html");
-});
-
-app.get("/3", (req, res) => {
-  res.sendFile(__dirname + "/public/theme3/index.html");
-});
-
-app.get("/4", (req, res) => {
-  res.sendFile(__dirname + "/public/theme4/index.html");
-});
 app.listen(8080, () => console.log("listening on port 8080"));
